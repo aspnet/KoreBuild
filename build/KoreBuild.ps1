e462a6fa8332a50d14049addd9cec0fa29135fbd
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2016-02-25 10:24:25 -0800

    Initial commit with the new build script

---
+++ build/KoreBuild.ps1
@@ -0,0 +1,70 @@
+$repoFolder = $env:REPO_FOLDER
+if (!$repoFolder) {
+    throw "REPO_FOLDER is not set"
+}
+
+Write-Host "Building $repoFolder"
+cd $repoFolder
+
+# Make the path relative to the repo root because Sake/Spark doesn't support full paths
+$koreBuildFolder = $PSScriptRoot
+$koreBuildFolder = $koreBuildFolder.Replace($repoFolder, "").TrimStart("\")
+
+$dotnetChannel = "beta"
+$dotnetVersion = "1.0.0.001496"
+
+if ($env:KOREBUILD_DOTNET_CHANNEL) 
+{
+    $dotnetChannel = $env:KOREBUILD_DOTNET_CHANNEL
+}
+if ($env:KOREBUILD_DOTNET_VERSION) 
+{
+    $dotnetVersion = $env:KOREBUILD_DOTNET_VERSION
+}
+if ($env:KOREBUILD_SKIP_RUNTIME_INSTALL -eq "1") 
+{
+    Write-Host "Skipping runtime installation because KOREBUILD_SKIP_RUNTIME_INSTALL = 1"
+}
+else
+{
+    $dotnetLocalInstallFolder = "$env:LOCALAPPDATA\Microsoft\dotnet\cli"
+    $dotnetLocalInstallFolderBin = "$dotnetLocalInstallFolder\bin"
+    & "$koreBuildFolder\dotnet\install.ps1" -Channel $dotnetChannel -Version $dotnetVersion
+
+    Write-Host "Adding $dotnetLocalInstallFolderBin to PATH"
+    $env:Path = "$dotnetLocalInstallFolderBin;$env:PATH"
+
+    # ==== Temporary =====
+    if ($env:SKIP_DNX_INSTALL -ne "1") 
+    {
+        $dnxVersion = "latest"
+        if ($env:BUILDCMD_DNX_VERSION) 
+        {
+            $dnxVersion = $env:BUILDCMD_DNX_VERSION
+        }
+
+        &"$koreBuildFolder\dnvm\dnvm.cmd" install $dnxVersion -runtime CoreCLR -arch x86 -alias default
+        &"$koreBuildFolder\dnvm\dnvm.cmd" install default -runtime CLR -arch x86 -alias default
+    }
+    else
+    {
+        &"$koreBuildFolder\dnvm\dnvm.cmd" use default -runtime CLR -arch x86
+    }
+    # ====================
+}
+
+if (!(Test-Path "$koreBuildFolder\Sake")) 
+{
+    &dotnet restore "$koreBuildFolder\project.json" --packages "$koreBuildFolder"
+}
+
+$makeFilePath = "makefile.shade"
+if (!(Test-Path $makeFilePath)) 
+{
+    $makeFilePath = "$koreBuildFolder\shade\makefile.shade"
+}
+
+Write-Host "Using makefile: $makeFilePath"
+
+$env:KOREBUILD_FOLDER=$koreBuildFolder
+&"$koreBuildFolder\Sake\0.2.2\tools\Sake.exe" -I $koreBuildFolder\shade -f $makeFilePath @args
--- build/KoreBuild.sh
+++ build/KoreBuild.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-[ -z "$KOREBUILD_DOTNET_CHANNEL" ] && KOREBUILD_DOTNET_CHANNEL=beta
-[ -z "$KOREBUILD_DOTNET_VERSION" ] && KOREBUILD_DOTNET_VERSION=1.0.0.001496
-
 targets=""
-filename=$0
+repoFolder=""
 while [[ $# > 0 ]]; do
     case $1 in
-        -m)
+        -r)
             shift
-            makeFilePath=$1
-            ;;
-        -n)
-            shift
-            nugetPath=$1
+            repoFolder=$1
             ;;
         *)
             targets+=" $1"
@@ -21,35 +14,23 @@ while [[ $# > 0 ]]; do
     esac
     shift
 done
-if [ ! -e "$nugetPath" ] || [ ! -e "$makeFilePath" ]; then
-    printf "Usage: $filename -m [makefile] -n [nuget] [ [targets] ]\n\n"
-    echo "       -m [makefile]  The makefile.shade to execute"
-    echo "       -n [nuget]     nuget.exe"
-    echo "       [targets]      A space separated list of targets to run"
+if [ ! -e "$repoFolder" ]; then
+    printf "Usage: $filename -r [repoFolder] [ [targets] ]\n\n"
+    echo "       -r [repo]     The repository to build"
+    echo "       [targets]     A space separated list of targets to run"
     exit 1
 fi
 
-# ==== find directory containing this file =====
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-thisDir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-# ===================
+echo "Building $repoFolder"
+cd $repoFolder
 
+# Make the path relative to the repo root because Sake/Spark doesn't support full paths
+koreBuildFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+koreBuildFolder="${koreBuildFolder/$repoFolder/}"
+koreBuildFolder="${koreBuildFolder#/}"
 
-sakeFolder=$thisDir/Sake
-if [ ! -d $sakeFolder ]; then
-    mono $nugetPath install Sake -ExcludeVersion -o $thisDir -nocache
-fi
-if [ ! -d $thisDir/xunit.runner.console ]; then
-    mono $nugetPath install xunit.runner.console -ExcludeVersion -o $thisDir -nocache
-fi
-if [ ! -d $thisDir/xunit.core ]; then
-    mono $nugetPath install xunit.core -ExcludeVersion -o $thisDir -nocache
-fi
+[ -z "$KOREBUILD_DOTNET_CHANNEL" ] && KOREBUILD_DOTNET_CHANNEL=beta
+[ -z "$KOREBUILD_DOTNET_VERSION" ] && KOREBUILD_DOTNET_VERSION=1.0.0.001496
 
 if [ ! -z "$KOREBUILD_SKIP_RUNTIME_INSTALL" ]; then
     echo "Skipping runtime installation because KOREBUILD_SKIP_RUNTIME_INSTALL is set"
@@ -58,12 +39,12 @@ else
     # requires sudo
     export DOTNET_INSTALL_DIR=~/.dotnet
     export PATH=$DOTNET_INSTALL_DIR/bin:$PATH
-    export KOREBUILD_FOLDER="$(dirname $thisDir)"
-    chmod +x $thisDir/dotnet-install.sh
-    $thisDir/dotnet-install.sh --channel $KOREBUILD_DOTNET_CHANNEL --version $KOREBUILD_DOTNET_VERSION
+    export KOREBUILD_FOLDER="$(dirname $koreBuildFolder)"
+    chmod +x $koreBuildFolder/dotnet/install.sh
+    $koreBuildFolder/dotnet/install.sh --channel $KOREBUILD_DOTNET_CHANNEL --version $KOREBUILD_DOTNET_VERSION
     # ==== Temporary ====
     if ! type dnvm > /dev/null 2>&1; then
-        source $thisDir/dnvm.sh
+        source $koreBuildFolder/dnvm/dnvm.sh
     fi
         if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
             dnvm install latest -runtime coreclr -alias default
@@ -90,4 +71,16 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 echo "Using Reference Assemblies from: $DOTNET_REFERENCE_ASSEMBLIES_PATH"
-mono $sakeFolder/tools/Sake.exe -I $thisDir -f $makeFilePath $targets
+
+sakeFolder=$koreBuildFolder/Sake
+if [ ! -d $sakeFolder ]; then
+    dotnet restore "$koreBuildFolder/project.json" --packages "$koreBuildFolder"
+fi
+
+makeFile="makefile.shade"
+if [ ! -e $makeFile ]; then
+    makeFile="$koreBuildFolder/shade/makefile.shade"
+fi
+
+export KOREBUILD_FOLDER="$koreBuildFolder"
+mono $sakeFolder/0.2.2/tools/Sake.exe -I $koreBuildFolder/shade -f $makeFile $targets
