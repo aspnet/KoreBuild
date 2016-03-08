e6b43938a89e6cf8b8575150cbb9f08d481a7aea
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-03-08 12:25:47 -0800

    Added the dnxtools feed to dotnet restore

--- build/KoreBuild.ps1
+++ build/KoreBuild.ps1
@@ -45,7 +45,7 @@ if (!($env:Path.Split(';') -icontains $dotnetLocalInstallFolderBin))
 if (!(Test-Path "$koreBuildFolder\Sake")) 
 {
     $toolsProject = "$koreBuildFolder\project.json"
-    &dotnet restore "$toolsProject" --packages "$koreBuildFolder" -v Minimal
+    &dotnet restore "$toolsProject" --packages "$koreBuildFolder" -f https://www.myget.org/F/dnxtools/api/v3/index.json -v Minimal
     # Rename the project after restore because we don't want it to be restore afterwards
     mv "$toolsProject" "$toolsProject.norestore"
     # We still nuget because dotnet doesn't have support for pushing packages
