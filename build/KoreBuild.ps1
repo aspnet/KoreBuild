142539693e5d9eb944e3be4a2b47921feb9b326b
Pranav K <prkrishn@hotmail.com>
2016-10-10 19:21:18 -0700

    Updating to shared runtime 1.1.0-preview1-001100-00

--- build/KoreBuild.ps1
+++ build/KoreBuild.ps1
@@ -15,7 +15,7 @@ $koreBuildFolder = $koreBuildFolder.Replace($repoFolder, "").TrimStart("\")
 $dotnetVersionFile = $koreBuildFolder + "\cli.version.win"
 $dotnetChannel = "preview"
 $dotnetVersion = Get-Content $dotnetVersionFile
-$dotnetSharedRuntimeVersion = "1.1.0-preview1-001090-00"
+$dotnetSharedRuntimeVersion = "1.1.0-preview1-001100-00"
 $dotnetSharedRuntimeChannel = "release/1.1.0"
 
 if ($env:KOREBUILD_DOTNET_CHANNEL) 
