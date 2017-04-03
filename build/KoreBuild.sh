07b38139539631cf58edbbc1c951cfe4249d23fa
Kiran Challa <kichalla@microsoft.com>
2017-04-03 12:13:45 -0700

    Upgrade CLI version to 2.0.0-preview1-005685

--- build/KoreBuild.sh
+++ build/KoreBuild.sh
@@ -123,7 +123,10 @@ else
     [[ ":$PATH:" != *":$DOTNET_INSTALL_DIR:"* ]] && export PATH="$DOTNET_INSTALL_DIR:$PATH"
 fi
 
-# Temporarily install 1.1.1 to prevent build breaks for repos not yet converted
+# Temporarily install these runtimes to prevent build breaks for repos not yet converted
+# 1.0.4 - for tools
+install_shared_runtime "1.0.4" "preview"
+# 1.1.1 - for test projects which haven't yet been converted to netcoreapp2.0
 install_shared_runtime "1.1.1" "release/1.1.0"
 
 if [ "$sharedRuntimeVersion" != "" ]; then
