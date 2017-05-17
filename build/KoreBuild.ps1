7730cc50e6078227c1e9b517458b86692adeb39e
Nate McMaster <nate.mcmaster@microsoft.com>
2017-05-17 11:09:42 -0700

    Unset ErrorActionPreference when invoking another process

--- build/KoreBuild.ps1
+++ build/KoreBuild.ps1
@@ -5,8 +5,11 @@ param([parameter(ValueFromRemainingArguments=$true)][string[]] $allparams)
 function __exec($cmd) {
     $cmdName = [IO.Path]::GetFileName($cmd)
     Write-Host -ForegroundColor Cyan "> $cmdName $args"
+    $originalErrorPref = $ErrorActionPreference
+    $ErrorActionPreference = 'Continue'
     & $cmd @args
     $exitCode = $LASTEXITCODE
+    $ErrorActionPreference = $originalErrorPref
     if($exitCode -ne 0) {
         throw "'$cmdName $args' failed with exit code: $exitCode"
     }
