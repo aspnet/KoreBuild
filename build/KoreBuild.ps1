67302d0b00061029945b8f29f6b8bc9da4834ff2
Kiran Challa <kichalla@microsoft.com>
2017-05-25 01:54:41 -0700

    Updated to install 1.1.2 version of shared runtime as the new CLIs look for it when a TFM is netcoreapp1.1.

--- build/KoreBuild.ps1
+++ build/KoreBuild.ps1
@@ -80,8 +80,8 @@ else
     # Temporarily install these runtimes to prevent build breaks for repos not yet converted
     # 1.0.4 - for tools
     InstallSharedRuntime -version "1.0.4" -channel "preview"
-    # 1.1.1 - for test projects which haven't yet been converted to netcoreapp2.0
-    InstallSharedRuntime -version "1.1.1" -channel "release/1.1.0"
+    # 1.1.2 - for test projects which haven't yet been converted to netcoreapp2.0
+    InstallSharedRuntime -version "1.1.2" -channel "release/1.1.0"
 
     if ($sharedRuntimeVersion)
     {
